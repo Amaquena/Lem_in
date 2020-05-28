75f235bc4127ca0d54a3066ab7ca718cc5bcf3e3
Kristin Trout <ktrout@student.wethinkcode.co.za>
2020-05-28 22:01:56 +0200

    added back deleted verify_paths function that fixed error management

--- lem-in.c
+++ lem-in.c
@@ -37,6 +37,41 @@ static void count_ants(t_farm *farm)
     and links in the farm struct.
 */
 
+int verify_paths(t_farm *farm)
+{
+    t_queue *path;
+    t_room *room;
+    int current_path;
+    int path_count;
+    int flag;
+
+    path_count = 0;
+    while (farm->paths[path_count])
+        path_count++;
+
+    current_path = 0;
+    while (current_path < path_count)
+    {
+        path = farm->paths[current_path];
+        flag = 0;
+        while (path)
+        {
+            room = find_room(path->name, farm->rooms);
+            if (room->type == START)
+                flag++;
+            if (room->type == END)
+                flag++;
+            path = path->next;
+        }
+        if (flag != 2)
+            return (0);
+        current_path++;
+    }
+    if (!path_count)
+        return (0);
+    return (1);
+}
+
 static void initialize_map(t_farm *farm)
 {
     int type;
@@ -57,17 +92,17 @@ static void initialize_map(t_farm *farm)
             verify_room(farm, type);
             type = REG;
         }
+        else if (ft_strchr(farm->line, '-') == NULL)
+            error_msg("Error: No links found.", farm);
         else if (ft_strchr(farm->line, '-') && farm->line[0] != '#')
             verify_links(farm);
-        // else if (ft_strchr(farm->line, '-') == NULL)
-        //     error_msg("Error: No links found.", farm);
         else if (farm->line[0] != '#')
             error_msg("Error: Input not recognized.", farm);
         ft_putendl(farm->line);
         ft_strdel(&farm->line);
     }
     if (ret < 0)
-        error_msg("Error: Failed to read file because file is empty.", farm);
+        error_msg("Error: Failed to read file.", farm);
     if (!verify_start_end(farm))
         error_msg("Error: No start or end declared.", farm);
     if (farm->ants < 0)
@@ -81,7 +116,10 @@ int main(void)
     intitailze_values(&farm);
     initialize_map(&farm);
     solve(&farm);
-    output_farm(&farm);
+    if (verify_paths(&farm))
+        output_farm(&farm);
+    else
+        error_msg("Error: Start room doesn't link to End.", &farm);
     free_farm(&farm);
     return (0);
 }
